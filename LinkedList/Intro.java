3601f0dcb0a0dc624f09647c3e37931d44f7218d
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-04-28 19:12:18 +0530

    ll

---
+++ LinkedList/Intro.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+public class Intro {
+  public static class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
+  // Methods-
+  // add()
+  // remove()
+  // print()
+  // search()
+
+  public static void main(String[] args) {
+    LinkedList ll = new LinkedList<>();
+
+  }
+}
