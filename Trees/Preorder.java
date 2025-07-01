4a1a16b1513d959d61ea116b3b14331e8ff6667a
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-07-01 15:06:23 +0530

    pre

--- Trees/Preorder.java
+++ Trees/Preorder.java
@@ -26,13 +26,24 @@ public class Preorder {
 
       return newNode;
     }
+
+    public static void preorder(Node root) { // O(n)
+      if (root == null) {
+        return;
+      }
+      System.out.print(root.data + " ");
+      preorder(root.left);
+      preorder(root.right);
+    }
   }
 
   public static void main(String[] args) {
     int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
     BinaryTree tree = new BinaryTree();
     Node root = tree.builTree(nodes);
-    System.out.println(root.data);
+    // System.out.println(root.data);
+
+    tree.preorder(root);
   }
 }
 
