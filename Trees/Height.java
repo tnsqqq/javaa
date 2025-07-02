d6616accd5b2707cbfe029bc389f999b2dc00326
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-07-02 15:20:14 +0530

    sum

--- Trees/Height.java
+++ Trees/Height.java
@@ -20,6 +20,26 @@ public class Height {
     return Math.max(lh, rh) + 1;
   }
 
+  public static int count(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int lc = count(root.left);
+    int rc = count(root.right);
+    return lc + rc + 1;
+  }
+
+  public static int sum(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int ls = sum(root.left);
+    int rs = sum(root.right);
+    return ls + rs + root.data;
+  }
+
   public static void main(String[] args) {
 
     // 1
@@ -37,5 +57,7 @@ public class Height {
     root.right.right = new Node(7);
 
     System.out.println(height(root));
+    System.out.println(count(root));
+    System.out.println(sum(root));
   }
 }
