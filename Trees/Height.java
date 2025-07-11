95a339515678c9da1ce9f2fd7d9fa7d9720059af
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-07-11 15:39:25 +0530

    transform

--- Trees/Height.java
+++ Trees/Height.java
@@ -272,6 +272,57 @@ public class Height {
 
   }
 
+  public static int kAncestor(Node root, int n, int k) {
+    if (root == null) {
+      return -1;
+    }
+
+    if (root.data == n) {
+      return 0;
+    }
+
+    int leftDist = kAncestor(root.left, n, k);
+    int rightDist = kAncestor(root.right, n, k);
+
+    if (leftDist == -1 && rightDist == -1) {
+      return -1;
+    }
+
+    int max = Math.max(leftDist, rightDist);
+
+    if (max + 1 == k) {
+      System.out.println(root.data);
+    }
+
+    return max + 1;
+  }
+
+  public static int transform(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftChild = transform(root.left);
+    int rightChild = transform(root.right);
+
+    int data = root.data;
+    int newLeft = root.left == null ? 0 : root.left.data;
+    int newRight = root.right == null ? 0 : root.right.data;
+
+    root.data = newLeft + leftChild + newRight + rightChild;
+    return data;
+  }
+
+  public static void preorder(Node root) {
+    if (root == null) {
+      return;
+    }
+
+    System.out.print(root.data + " ");
+    preorder(root.left);
+    preorder(root.right);
+  }
+
   public static void main(String[] args) {
 
     // 1
@@ -303,11 +354,17 @@ public class Height {
     // int k = 2;
     // KLevel(root, 1, k);
 
-    int n1 = 4, n2 = 6;
+    // int n1 = 4, n2 = 6;
     // System.out.println(lca(root, n1, n2).data);
     // System.out.println(lca2(root, n1, n2).data);
 
-    System.out.println(minDist(root, n1, n2));
+    // System.out.println(minDist(root, n1, n2));
+
+    // int n = 5, k = 2;
+    // kAncestor(root, n, k);
+
+    transform(root);
+    preorder(root);
 
   }
 }
