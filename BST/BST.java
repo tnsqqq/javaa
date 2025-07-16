7ee9f94586bacf23c0a70a0dcf5372595dfb6b42
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-07-16 14:48:33 +0530

    print root to leaf

--- BST/BST.java
+++ BST/BST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BST {
 
   static class Node {
@@ -103,8 +105,31 @@ public class BST {
 
   }
 
+  public static void printPath(ArrayList<Integer> path) {
+    for (int i = 0; i < path.size(); i++) {
+      System.out.print(path.get(i) + "->");
+    }
+    System.out.println("Null");
+  }
+
+  public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+    if (root == null) {
+      return;
+    }
+
+    path.add(root.data);
+    if (root.left == null && root.right == null) {
+      printPath(path);
+    }
+
+    printRoot2Leaf(root.left, path);
+    printRoot2Leaf(root.right, path);
+    path.remove(path.size() - 1);
+
+  }
+
   public static void main(String[] args) {
-    int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+    int values[] = { 8, 5, 3, 6, 10, 11, 14 };
     Node root = null;
 
     for (int i = 0; i < values.length; i++) {
@@ -125,6 +150,8 @@ public class BST {
 
     // inorder(root);
 
-    printInRange(root, 5, 12);
+    // printInRange(root, 5, 12);
+
+    printRoot2Leaf(root, new ArrayList<>());
   }
 }
