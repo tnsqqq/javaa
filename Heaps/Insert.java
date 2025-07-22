81e5dca297ae89e2d8b5631c53e8a8fe96596df6
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-07-22 18:15:42 +0530

    insert in heap

---
+++ Heaps/Insert.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Insert {
+
+  static class Heap {
+    ArrayList<Integer> arr = new ArrayList<>();
+
+    public void add(int data) {
+      // add at last index
+      arr.add(data);
+
+      int x = arr.size() - 1; // x is child index
+      int par = (x - 1) / 2; // par index / parent of x
+
+      while (arr.get(x) < arr.get(par)) { // O(logn)
+        // swap
+        int temp = arr.get(x);
+        arr.set(x, arr.get(par));
+        arr.set(par, temp);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+
+    Heap heap = new Heap();
+
+    heap.add(10);
+    heap.add(5);
+    heap.add(20);
+    System.out.println(heap.arr);
+
+  }
+}
